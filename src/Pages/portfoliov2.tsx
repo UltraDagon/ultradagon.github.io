78642615178b0ff4d04c032acc91415310df5005
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2025-06-29 23:24:38 -0500

    fixed deleteme popup not dissapearing upon route change. Also limited popups to be 1 per title

--- src/Pages/portfoliov2.tsx
+++ src/Pages/portfoliov2.tsx
@@ -2,22 +2,31 @@ import NavWindowBox from "../components/NavWindowBox";
 import PopUpWindow from "../components/PopUpWindow";
 import "./portfoliov2.css";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function PortfolioV2() {
+  const location = useLocation(); // Used to tell when the site changes to get rid of the deleteme popup
   const [popUpWindows, setPopUpWindows] = useState([{ title: "deleteme" }]);
 
   function createPopUp(title: string) {
-    setPopUpWindows([...popUpWindows, { title: title }]);
+    // If window does not already exist
+    if (-1 == popUpWindows.findIndex((e) => e.title == title)) {
+      // Make a new pop up window
+      setPopUpWindows([...popUpWindows, { title: title }]);
+    }
+    // TODO: do some kind of alert if the window already exists
   }
 
   function removePopUp(title: string) {
+    console.log("hello");
     setPopUpWindows(popUpWindows.filter((e) => e.title != title));
   }
 
-  window.onload = (event) => {
+  //Deletes the deleteme popup at whenever the page is loaded
+  useEffect(() => {
     removePopUp("deleteme");
-  };
+  }, [location]);
 
   return (
     <>
