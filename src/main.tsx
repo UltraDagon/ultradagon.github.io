9a03a49ebe4a73c318d228f5c6bbb9f75e6ed615
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2025-08-21 15:30:00 -0500

    Tried putting routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Pages/home";
+import AllProjects from "./Pages/all_projects";
+import PortfolioV2 from "./Pages/portfoliov2";
+
+import NavBar from "./components/NavBar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/all-projects" element={<AllProjects />} />
+        <Route path="/portfoliov2" element={<PortfolioV2 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
