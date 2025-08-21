8adb8db9e77d41e76309fb06f6a1a5cda4e5beed
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2025-08-21 14:50:44 -0500

    Browser router  fix by removing the layout one

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/home";
 import About from "./Pages/about";
 import Blog from "./Pages/blog";
@@ -12,22 +7,22 @@ import CurrentProjects from "./Pages/current_projects";
 import Layout from "./layout";
 import APWebsite from "./Pages/projects/website";
 import PortfolioV2 from "./Pages/portfoliov2";
+import NavBar from "./components/NavBar";
 
 // Note for Dagon: npm run deploy -- -m "message"
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/current-projects" element={<CurrentProjects />} />
-          <Route path="/all-projects" element={<AllProjects />} />
-          <Route path="/all-projects/website" element={<APWebsite />} />
-          <Route path="/portfoliov2" element={<PortfolioV2 />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/current-projects" element={<CurrentProjects />} />
+        <Route path="/all-projects" element={<AllProjects />} />
+        <Route path="/all-projects/website" element={<APWebsite />} />
+        <Route path="/portfoliov2" element={<PortfolioV2 />} />
       </Routes>
     </BrowserRouter>
   );
